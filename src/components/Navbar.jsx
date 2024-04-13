734c3c782f78fd816915ca146f6ddea3a8f2b3aa
brosmar18 <bryangonzalez1896@icloud.com>
2024-04-12 23:28:29 -0500

    Navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,26 @@ import {
   Stack,
   Link,
   Box,
+  InputBase,
+  Button,
+  Badge,
 } from "@mui/material";
-import { Facebook, LinkedIn, Twitter, Mail } from "@mui/icons-material";
+import {
+  Facebook,
+  LinkedIn,
+  Twitter,
+  Mail,
+  Favorite,
+  ShoppingCart,
+} from "@mui/icons-material";
 import { navLinks } from "../constants";
+import { logo } from "../assets";
 
 const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+const BottomToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
@@ -19,21 +34,35 @@ const SocialIcons = styled(Box)({
   display: "flex",
   gap: "16px",
 });
+
+const ShopIcons = styled(Box)({
+  display: "flex",
+  gap: "18px",
+});
+
 const LinksContainer = styled(Box)({
   display: "flex",
   gap: "20px",
   justifyContent: "flex-end",
 });
 
+const Search = styled(Box)(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "12px 16px",
+  borderRadius: theme.shape.borderRadius,
+  width: "36.25rem",
+}));
+
 const Navbar = () => {
   return (
-    <AppBar position="sticky">
-      <Stack
-        sx={{
-          padding: "8px 100px",
-          backgroundColor: "black",
-        }}
-      >
+    <AppBar
+      position="sticky"
+      sx={{
+        padding: "8px 100px 24px",
+        backgroundColor: "black",
+      }}
+    >
+      <Stack spacing={2}>
         <StyledToolbar>
           <SocialIcons>
             <Facebook fontSize="small" />
@@ -59,7 +88,29 @@ const Navbar = () => {
             ))}
           </LinksContainer>
         </StyledToolbar>
-        <StyledToolbar></StyledToolbar>
+        <BottomToolbar>
+          <img src={logo} alt="logo" width={84} height={84} />
+          <Box sx={{ display: "flex", gap: "4px" }}>
+            <Search>
+              <InputBase
+                placeholder="What are you looking for?"
+                sx={{ width: "100%" }}
+              />
+            </Search>
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "#53B3F8", padding: "12px 24px" }}
+            >
+              Search
+            </Button>
+          </Box>
+          <ShopIcons>
+            <Favorite fontSize="large" />
+            <Badge badgeContent={4} color="error">
+              <ShoppingCart fontSize="large" />
+            </Badge>
+          </ShopIcons>
+        </BottomToolbar>
       </Stack>
     </AppBar>
   );
