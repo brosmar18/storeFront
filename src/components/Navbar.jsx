618ea44a6cd58959fe532b52e5b4303df0e05ace
brosmar18 <bryangonzalez1896@icloud.com>
2024-04-12 22:49:31 -0500

    Topbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,66 @@
-import { AppBar, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Typography,
+  Stack,
+  Link,
+  Box,
+} from "@mui/material";
+import { Facebook, LinkedIn, Twitter, Mail } from "@mui/icons-material";
+import { navLinks } from "../constants";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const SocialIcons = styled(Box)({
+  display: "flex",
+  gap: "16px",
+});
+const LinksContainer = styled(Box)({
+  display: "flex",
+  gap: "20px",
+  justifyContent: "flex-end",
+});
 
 const Navbar = () => {
   return (
     <AppBar position="sticky">
-      <Toolbar></Toolbar>
+      <Stack
+        sx={{
+          padding: "8px 100px",
+          backgroundColor: "black",
+        }}
+      >
+        <StyledToolbar>
+          <SocialIcons>
+            <Facebook fontSize="small" />
+            <LinkedIn fontSize="small" />
+            <Twitter fontSize="small" />
+            <Mail fontSize="small" />
+          </SocialIcons>
+          <LinksContainer>
+            {navLinks.map((link) => (
+              <Link
+                key={link.label}
+                href={link.route}
+                color="primary"
+                underline="none"
+                sx={{
+                  fontSize: "16px",
+                  color: "#f1f1f1",
+                  fontWeight: "500",
+                }}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </LinksContainer>
+        </StyledToolbar>
+        <StyledToolbar></StyledToolbar>
+      </Stack>
     </AppBar>
   );
 };
---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    label: "Introduce",
+    route: "#",
+  },
+  {
+    label: "Partner Incentives",
+    route: "#",
+  },
+  {
+    label: "Promotion",
+    route: "#",
+  },
+  {
+    label: "Contact",
+    route: "#",
+  },
+  {
+    label: "Frequently asked questions",
+    route: "#",
+  },
+];
