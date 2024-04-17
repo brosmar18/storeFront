2f59efc5ceb811a6b020e057c39eb87aac6744b0
brosmar18 <bryangonzalez1896@icloud.com>
2024-04-17 09:34:32 -0500

    Responsive styles added

--- src/sections/Trending/Trending.jsx
+++ src/sections/Trending/Trending.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import "./Trending.scss";
 
 const Trending = () => {
-  return <div>Trending</div>;
+  return (
+    <section className="trending">
+      {/* Heading  */}
+      <div className="trending__heading">
+        <div className="trending__heading-title">
+          <p className="subTitle">Trending</p>
+          <div className="title">
+            <h2>Trending Now</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+          </div>
+        </div>
+        <button type="button">View All</button>
+      </div>
+      {/* Cards  */}
+      <div className="trending__cards">
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Trending;
