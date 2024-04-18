375162719b34a4a8a09d0fe4dff5a9d36c5f17cb
brosmar18 <bryangonzalez1896@icloud.com>
2024-04-18 06:09:58 -0500

    Catalog section added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
 import logo from "./logo.svg";
+import gameCollection from "./gameCollection.png";
 
-
-export { logo };
+export { logo, gameCollection };
--- src/sections/Catalog/Catalog.jsx
+++ src/sections/Catalog/Catalog.jsx
@@ -1,7 +1,31 @@
+import { gameCollection } from "../../assets";
 import "./Catalog.scss";
 
 const Catalog = () => {
-  return <div>Catalog</div>;
+  return (
+    <section className="catalog">
+      {/* Image  */}
+      <div className="catalog__image">
+        <img src={gameCollection} alt="game colection" />
+      </div>
+      {/* Text  */}
+      <div className="catalog__text">
+        <div className="catalog__text-container">
+          <div className="heading">
+            <p className="subTitle">Catalog</p>
+            <h2 className="title">Explore Our Catalog</h2>
+          </div>
+          <p className="desc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+            leo porta nunc, velit. Dui justo, non platea viverra aliquam
+            convallis arcu molestie egestas. Volutpat integer tristique ac nam
+            integer. Hac ut nisi, ut amet donec porta.
+          </p>
+          <button className="button">Learn More</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Catalog;
