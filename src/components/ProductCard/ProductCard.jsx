687850a4584ad0d18262d0c3baf3cc1297844388
brosmar18 <bryangonzalez1896@icloud.com>
2024-04-26 18:39:46 -0500

    shopProducts by category section added

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,17 +1,27 @@
+import { pikachu } from "../../assets/gameCovers";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./ProductCard.scss";
 
-const ProductCard = ({ title, description, price }) => {
+const ProductCard = () => {
   return (
     <div className="productCard">
-      <div className="productCard__image" />
-      <div className="productCard__details">
-        <h2>Title</h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-        </p>
-        <div className="productCard__footer">
-          <span>59.99</span>
-          <button>Add to Cart</button>
+      <img src={pikachu} alt="pikachu" />
+      <div className="productCard__footer">
+        <div className="productCard__info">
+          <h3>Title</h3>
+          <div className="price">
+            <p>$49.99</p>
+            <p>$39.99</p>
+          </div>
+        </div>
+        <div className="buttons">
+          <button type="button">
+            <ShoppingCartIcon />
+          </button>
+          <button type="button">
+            <FavoriteIcon />
+          </button>
         </div>
       </div>
     </div>
--- src/sections/Shop/ShopProducts/ShopProducts.jsx
+++ src/sections/Shop/ShopProducts/ShopProducts.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import "./ShopProducts.scss";
+import { ProductCard } from "../../../components";
+import { gameCategories } from "../../../constants";
+import { Select, MenuItem } from "@mui/material";
 
 const ShopProducts = () => {
   return (
     <section className="shopProducts">
-      <div className="shopProducts__container">
-        {/* Header  */}
-        <div className="shopProducts__heading">
-          <div className="heading-title">
-            <h2>Browse Our Games</h2>
-          </div>
-          <div className="menu">
-            <a href="#">Categories</a>
-          </div>
+      {/* HEADER  */}
+      <div className="shopProducts__heading">
+        <div className="shopProducts__heading-title">
+          <p className="subTitle">By Category</p>
+          <h2>Our Games</h2>
         </div>
-        {/* Cards  */}
+        <div className="shopProducts__categories">
+          <Select defaultValue="all" className="category-select">
+            <MenuItem value="all">All Categories</MenuItem>
+            {gameCategories.map((item) => (
+              <MenuItem key={item.category}>{item.category}</MenuItem>
+            ))}
+          </Select>
+        </div>
+      </div>
+      {/* CARDS  */}
+      <div className="shopProducts__cards">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </section>
   );
