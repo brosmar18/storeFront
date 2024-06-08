32c1f040ff432e1eb4b12fc69800260e67a2972a
brosmar18 <bryangonzalez1896@icloud.com>
2024-06-08 16:25:21 -0500

    phaseFour. Next.js playground

--- src/sections/Shop/index.js
+++
@@ -1,5 +0,0 @@
-import ShopHero from "./ShopHero/ShopHero";
-import ShopCategories from "./ShopCategories/ShopCategories";
-import ShopProducts from "./ShopProducts/ShopProducts";
-
-export { ShopHero, ShopCategories, ShopProducts };
